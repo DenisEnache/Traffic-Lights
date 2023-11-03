9789cf37c56cbeb127d0091ad0b149275bbb41f2
DenisEnache <149329298+DenisEnache@users.noreply.github.com>
2023-11-03 13:33:53 +0000

    Update script.js - eventListener eliminated

--- script.js
+++ script.js
@@ -2,14 +2,9 @@ const button = document.getElementById("Button");
 const colors = ["yellow", "green", "red"];
 let clicks = 0;
 
-button.addEventListener("click", function Onclick() {
-    button.style.backgroundColor = colors[clicks];
-    clicks = (clicks + 1) % colors.length;
-})
-
-function colorChange() {
+function changeColor() {
     button.style.backgroundColor = colors[clicks];
     clicks = (clicks + 1) % colors.length;
 }
 
-setInterval(colorChange, 10000);
+setInterval(changeColor, 10000);
